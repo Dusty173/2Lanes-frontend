be54d24bc318c2b81511995753d2b7929bf53de3
Dustin <dustincheyney@gmail.com>
2024-08-25 12:35:20 -0700

    Added loading animation

--- src/common/LoadIcon.js
+++ src/common/LoadIcon.js
@@ -4,7 +4,14 @@ import "./LoadIcon.css";
 /** Loading message used by components that fetch API data. */
 
 function LoadIcon() {
-  return <div className="LoadIcon">Loading ...</div>;
+  return (
+  <>
+    <div className="LoadIcon"></div>;
+    <div className="LoadText">This may take a moment...</div>
+  </>)
+  
+  
+
 }
 
 export default LoadIcon;
